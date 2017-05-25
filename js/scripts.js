39246450c6e1c58347c93e1f64d4d9c7ffe6b3f3
julian flood and jusin gomez <jusin@>
2017-05-25 14:48:57 -0700

    finish project

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,18 @@ function Entry(movie, year, score){
   this.year = year;
   this.score = score;
 }
-var movies = [];
+
 
 //UI
 $(document).ready(function() {
 
+  var movies = [];
+
   $('form').submit(function(event) {
     event.preventDefault();
+
+
+
 
     var movieTitle = $('#movie-input').val();
     var movieYear = $('#year-input').val();
@@ -20,23 +25,33 @@ $(document).ready(function() {
 
     movies.push(movieArray);
 
+    if((movieArray.year === "") || (movieArray.movie === "") || (movieArray.score === NaN)) {
+      alert("Please Enter");
+      return false;
+    }
+    console.log(movieArray);
+
     $("#table").append("<tr>" + "<td>" + movieTitle + "</td>"  + "<td>" + movieYear + "</td>" + "<td>" + tomatoScore + "</td>" + "</tr>");
+      $(this).closest('form').find("input[type=input]").val("");
+
+      $('#sendSort').show();
   });
 
 
   $('#sendSort').click(function(event) {
     event.preventDefault();
-
     var newSorted = movies.sort(function(a, b) {
       return b.score - a.score
     });
-
-
+    $("#table").hide();
+    $("#sorted").show();
     newSorted.forEach(function(item){
       $("#sorted").append("<tr>" + "<td>" + item.movie + "</td>"  + "<td>" + item.year + "</td>" + "<td>" + item.score + "</td>" + "</tr>");
       console.log(newSorted);
-    })
 
+    })
+    $("#sendSort").hide();
+    movies = [];
   });
 
 
